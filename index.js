3014f8f4d7f2bb9ec6fa67af57b1e279069f273b
Lê Tấn Trường <57832500+letantruong197@users.noreply.github.com>
2021-09-13 16:49:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let currentUser = localStorage.getItem("uid")
-if (!currentUser){
-    location.replace("https://letantruong197.github.io/du-an-cuoi-khoa-WI/login.html")
-}
+// let currentUser = localStorage.getItem("uid")
+// if (!currentUser){
+//     location.replace("https://letantruong197.github.io/du-an-cuoi-khoa-WI/login.html")
+// }
